2102922f0dacf6f315c03a8b190f65d957211055
iglesio <iglesio_santana@hotmail.com>
2024-09-18 07:37:01 -0300

    Add some comments

--- Inventory.ps1
+++ Inventory.ps1
@@ -1,4 +1,9 @@
-﻿$hostname = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
+<#
+This powershell script collects information about a computer and export this information on a report in HTML.
+#>
+
+#CIM querys to collect information about the computer.
+$hostname = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
 $serialnumber = Get-CimInstance -ClassName Win32_BIOS | Select-Object -expandproperty Serialnumber 
 $manufacturer = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expandproperty Manufacturer 
 $model = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expandproperty Model
@@ -7,25 +12,24 @@ $os = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -expandpr
 $memory = (Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object -expandproperty capacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum) / 1GB
 $disks = Get-CimInstance -ClassName Win32_logicaldisk -filter "DriveType='3'" | Select-Object DeviceID, @{Name="SizeGB";Expression={$_.Size/1GB -as [int]}}, @{Name="FreeDiskSpace";Expression={[math]::Round($_.FreeSpace/1GB,2)}}, @{Name="FreeDiskSpacePercentage";Expression={[Math]::Round(($_.FreeSpace / $_.Size * 100),0)}} 
 $softwares = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
-$softwares += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$softwares += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion
 #$softwares | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
 
-#Conta quantos discos tem no host
+#Count how many disks the computer have.
 $diskcount = $disks | Measure-Object | Select-Object Count
 $qtddisk = $diskcount.Count
-#Zera o array de discos
+#Clear the disk array
 $diskfull = $null
 
-#Percorre o objeto disks e extrai somente a descrição do disco e o tamanho e converte para texto para ser exportado no csv.
+#Search the disk array and extract the disk description and size and join this information on a single string.
 for (($i=0); $i -lt $qtddisk; $i++){    
     $DeviceID = $disks[$i].DeviceID
     $SizeGB = $disks[$i].SizeGB
     $diskfull += (@("$DeviceID $SizeGB") -join '' )+"GB " 
 }
 
-#Função que verifica a quantidade de cpu e adiciona quantidade na frente
-function CalculaCPU{
-    #Conta a quantidade de cpus
+#Function to calculate the number of cpus.
+function CalculateCPU{
     $qtdcpu = $processor.count
     $cpu = $processor[0] 
     if ($qtdcpu -gt 1){
@@ -35,7 +39,7 @@ function CalculaCPU{
         }
 }
 #Chama a função CalculaCPU
-$cputotal = CalculaCPU
+$cputotal = CalculateCPU
 
 Write-Output $hostname
 Write-Output $serialnumber
