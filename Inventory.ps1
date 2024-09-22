ac9827451e286811ae12f73396a027ec30212971
iglesio <iglesio_santana@hotmail.com>
2024-09-22 18:37:29 -0300

    Exported info as an HTML file and added more information to the inventory

--- Inventory.ps1
+++ Inventory.ps1
@@ -3,15 +3,21 @@ This powershell script collects information about a computer and export this inf
 #>
 
 #CIM querys to collect information about the computer.
-$hostname = (Get-CimInstance -ClassName Win32_ComputerSystem).Name
-$serialnumber = Get-CimInstance -ClassName Win32_BIOS | Select-Object -expandproperty Serialnumber 
-$manufacturer = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expandproperty Manufacturer 
-$model = Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object -expandproperty Model
+$computersystem = Get-CimInstance -ClassName Win32_ComputerSystem
+$hostname = $computersystem.Name
+$username = $computersystem.PrimaryOwnerName
+$domain = $computersystem.Domain
+$manufacturer = $computersystem.Manufacturer
+$model = $computersystem.Model
+$bios = Get-CimInstance -ClassName Win32_BIOS
+$serialnumber = $bios.SerialNumber
+$biosversion = $bios.BIOSVersion -join " "
 $processor = Get-CimInstance -ClassName Win32_processor | Select-Object -expandproperty name
 $os = Get-CimInstance -ClassName Win32_OperatingSystem | Select-Object -expandproperty caption
 $memory = (Get-CimInstance -ClassName Win32_PhysicalMemory | Select-Object -expandproperty capacity | Measure-Object -Sum | Select-Object -ExpandProperty Sum) / 1GB
-$disks = Get-CimInstance -ClassName Win32_logicaldisk -filter "DriveType='3'" | Select-Object DeviceID, @{Name="SizeGB";Expression={$_.Size/1GB -as [int]}}, @{Name="FreeDiskSpace";Expression={[math]::Round($_.FreeSpace/1GB,2)}}, @{Name="FreeDiskSpacePercentage";Expression={[Math]::Round(($_.FreeSpace / $_.Size * 100),0)}} 
-$softwares = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate
+$disks = Get-CimInstance -ClassName Win32_logicaldisk -filter "DriveType='3'" #| Select-Object DeviceID, @{Name="SizeGB";Expression={$_.Size/1GB -as [int]}}, @{Name="FreeDiskSpace";Expression={[math]::Round($_.FreeSpace/1GB,2)}}, @{Name="FreeDiskSpacePercentage";Expression={[Math]::Round(($_.FreeSpace / $_.Size * 100),0)}} 
+$timezone = Get-CimInstance -ClassName Win32_TimeZone | Select-Object -expandproperty Caption
+$softwares = Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion
 $softwares += Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion
 #$softwares | ?{ $_.DisplayName -ne $null } | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
 
@@ -28,8 +34,21 @@ for (($i=0); $i -lt $qtddisk; $i++){
     $diskfull += (@("$DeviceID $SizeGB") -join '' )+"GB " 
 }
 
+#Function to calculate the number of disks.
+function GetDisks{
+    $diskinfo = @()
+    foreach ($disk in $disks){
+        $diskinfo = [PSCustomObject]@{
+            Partition = $disk.DeviceID
+            Size = "{0:N2}GB" -f [math]::round($disk.Size / 1GB, 2)
+            FreeSpace = "{0:N2}GB" -f [math]::round($disk.FreeSpace / 1GB, 2)
+            FreeSpacePercentage = "{0:N2}%" -f [math]::round($disk.FreeSpace / $disk.Size * 100, 2)
+        }
+    }
+}
+
 #Function to calculate the number of cpus.
-function CalculateCPU{
+function GetCPU{
     $qtdcpu = $processor.count
     $cpu = $processor[0] 
     if ($qtdcpu -gt 1){
@@ -38,18 +57,72 @@ function CalculateCPU{
         return $processor
         }
 }
-#Chama a função CalculaCPU
+#Execute function CalculateCPU
 $cputotal = CalculateCPU
 
-Write-Output $hostname
-Write-Output $serialnumber
-Write-Output $cputotal
-Write-Output $os
-Write-Output $memory'GB'
-Write-Output $manufacturer
-Write-Output $model
-Write-Output $diskfull
-Write-Output $softwares | sort-object -Property DisplayName -Unique | Format-Table -AutoSize
+$htmlcontent = @"
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inventory 2024</title>
+
+<style>    
+#table {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+#table td, #table th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#table tr:nth-child(even){background-color: #f2f2f2;}
+
+#table tr:hover {background-color: #ddd;}
+
+#table th{
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: center;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+</head>
+<body>
+    <header>
+    </header> 
+
+    <main>
+        <table id="table">
+            <tr><th colspan=2>Inventory 2024</th></tr>
+            <tr><td><b>Hostname</b></td><td>$hostname</td></tr>
+            <tr><td><b>Username</b></td><td>$username</td></tr>
+            <tr><td><b>Domain</b></td><td>$domain</td></tr>
+            <tr><td><b>Manufacturer</b></td><td>$manufacturer</td></tr>
+            <tr><td><b>Model</b></td><td>$model</td></tr>
+            <tr><td><b>Bios Version</b></td><td>$biosversion</td></tr>
+            <tr><td><b>SerialNumber</b></td><td>$serialnumber</td></tr>
+            <tr><td><b>Processor</b></td><td>$processor</td></tr>
+            <tr><td><b>OS</b></td><td>$os</td></tr>
+            <tr><td><b>Memory</b></td><td>$memory</td></tr>
+            <tr><td><b>Disks</b></td><td>$diskfull</td></tr>
+            <tr><td><b>Timezone</b></td><td>$timezone</td></tr>
+            <tr><td><b>CPUs</b></td><td>$cputotal</td></tr>
+        </table>
+    </main>
+
+    <footer id="footer">
+    </footer>
+</body>
+</html>
+"@
+
+$htmlcontent | Out-File "C:\Users\$username\$hostname.html"
 
 
 
